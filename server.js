9cea598f52c6514c3a32c2c59152a3e5bdf86289
hemani167 <31912867+hemani167@users.noreply.github.com>
2017-09-17 14:42:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',fuction (req, res) {
-    res.send('Article-one requested and will be serve here');
+  app.get('/article-one',fuction(req,res) );
+    res.send('article-one requested and will be serve here');
 });  
     app.get('/article-two',fuction(req,res) {
     res.send('Article-two requested and will be serve here');
